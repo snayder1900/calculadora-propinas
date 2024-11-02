3efe147c2e50438a4fa6d15f05f702aca7027ff2
snider <snaydergp@hotmail.com>
2024-11-02 10:24:22 -0500

    proyecto terminado

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,18 @@
+import type { MenuItem } from "../types";
+
+type MenuItemProps = {
+  item: MenuItem;
+  addItem: (item: MenuItem) => void;
+};
+
+export default function MenuItem({ item, addItem }: MenuItemProps) {
+  return (
+    <button
+      onClick={() => addItem(item)}
+      className="flex justify-between border-2 border-teal-400 hover:bg-teal-200 w-full p-3"
+    >
+      <p>{item.name}</p>
+      <p className="font-black">${item.price}</p>
+    </button>
+  );
+}
---
+++ src/components/TipPercentageForm.tsx
@@ -0,0 +1,47 @@
+import type { Dispatch, SetStateAction } from "react";
+
+const tipOptions = [
+  {
+    id: "tip-10",
+    value: 0.1,
+    label: "10%",
+  },
+  {
+    id: "tip-20",
+    value: 0.2,
+    label: "20%",
+  },
+  {
+    id: "tip-50",
+    value: 0.5,
+    label: "50%",
+  },
+];
+
+type TipPercentageFormProps = {
+  setTip: Dispatch<SetStateAction<number>>;
+  tip: number;
+};
+
+export const TipPercentageForm = ({ setTip, tip }: TipPercentageFormProps) => {
+  return (
+    <div>
+      <h3 className="font-black text-2xl">Propina:</h3>
+      <form>
+        {tipOptions.map((tipOption) => (
+          <div className="flex gap-2" key={tipOption.id}>
+            <label htmlFor={tipOption.id}>{tipOption.label}</label>
+            <input
+              type="radio"
+              name="tip"
+              value={tipOption.value}
+              id={tipOption.id}
+              onChange={(e) => setTip(+e.target.value)}
+              checked={tipOption.value === tip}
+            />
+          </div>
+        ))}
+      </form>
+    </div>
+  );
+};
